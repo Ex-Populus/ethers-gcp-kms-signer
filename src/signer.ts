f11819608ca44e5e7528016b026f85e362c1006c
Spencer Gray <spencer@expopulus.com>
2024-12-05 12:41:34 -0800

    fix: force update

--- src/signer.ts
+++ src/signer.ts
@@ -30,7 +30,7 @@ export class GcpKmsSigner extends ethers.Signer {
 
   ethereumAddress: string;
 
-  forceRelease3: boolean;
+  forceRelease4: boolean;
 
   constructor(kmsCredentials: GcpKmsSignerCredentials, provider?: ethers.providers.Provider) {
     super();
