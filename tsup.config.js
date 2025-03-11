0fc5b28a549bf9306c7e57c9acc74bd5dd4b01ce
Christopher Powroznik <christopher@expopulus.com>
2025-03-10 17:04:52 -0700

    chore: update dependencies and configuration
    - Add `tsup.config.js` to `.npmignore`
    - Update `package-lock.json` with new and updated packages
    - Include various esbuild, rollup, and other related modules
    - Upgrade versions for existing dependencies like `cross-spawn`, `debug`, etc.
    - Introduce new dev dependencies such as `consola`, `joycon`, and others

---
+++ tsup.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/signer.ts"], // entry point
+  format: ["esm", "cjs"], // for both CommonJS and ESModule outputs
+  target: "es2020", // Keeps the output in modern syntax
+  dts: true, // generate .d.ts files for TypeScript
+  sourcemap: true,
+  minify: false,
+  clean: true,
+  metafile: true,
+  platform: "node",
+  splitting: false,
+});
